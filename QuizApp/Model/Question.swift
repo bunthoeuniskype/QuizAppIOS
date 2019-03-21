be97f20336b56f0f7508e86ea3fe2dfd9416a0b2
Admin <admin@Admins-Mac.local>
2019-03-22 00:16:04 +0700

    quizapptest

---
+++ QuizApp/Model/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  QuizApp
+//
+//  Created by Admin on 3/21/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ QuizApp/Model/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  QuizApp
+//
+//  Created by Admin on 3/21/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
